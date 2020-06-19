0a3f4f877d0282bfc988c3ed36ac16e7bef486b4
Denis Vitor Bueno de Almeida <denisvba@motorola.com>
2020-06-19 02:55:35 -0300

    chapter 4c -  LetterSearch - More conditions

--- src/chapter4/LetterSearch.java
+++ src/chapter4/LetterSearch.java
@@ -19,11 +19,13 @@ public class LetterSearch {
         boolean letterFound = false;
 
         //Search text for letter A
-        for (int count=0; count<text.length(); count++){
-            char currentLetter = text.charAt(count);
+        //for (int count=0; count<text.length(); count++){
+        //for (int count = text.length(); count >0;  count--){
+        for (int count=0; !letterFound && count<text.length(); count++){
+                char currentLetter = text.charAt(count);
             if(currentLetter == 'A' || currentLetter == 'a'){
                 letterFound = true;
-                break;
+                //break;
             }
         }
 
